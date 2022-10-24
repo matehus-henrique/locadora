aa1496d49656a6140bae86e38ee6fa6c937f94eb
matehus-henrique <mtheusriki@gmail.com>
2022-10-24 12:01:58 -0300

    Criando um componente Vue para cards

---
+++ resources/js/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- início do card de busca -->
+    <div class="card mb-3">
+                    <div class="card-header">{{titulo}}</div>
+
+                    <div class="card-body">
+                       <slot name="conteudo"></slot>
+
+                    </div>
+
+                    <div class="card-footer">
+                        <slot name="rodape"></slot>
+                    </div>
+                </div>
+                <!-- fim do card de busca -->
+</template>
+
+<script>
+    export default {
+        props: ['titulo']
+    }
+</script>
---
+++ resources/js/components/Marcas.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                
+                
+                <!-- início do card de busca -->
+                <card-component titulo="Busca de marcas">
+                    <template v-slot:conteudo>
+                        <div class="form-row">
+                            <div class="col mb-3">
+                                <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. Informe o ID da marca">
+                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                                </input-container-component>                   
+                            </div>
+                            <div class="col mb-3">
+                                <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp" texto-ajuda="Opcional. Informe o nome da marca">
+                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                                </input-container-component>
+                            </div>
+                        </div>
+                    </template>
+                    
+                    <template v-slot:rodape>
+                        <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
+                    </template>
+                </card-component>
+                <!-- fim do card de busca -->
+
+
+                <!-- início do card de listagem de marcas -->
+                <card-component titulo="Relação de marcas">
+                    <template v-slot:conteudo>
+                        <table-component></table-component>
+                    </template>
+
+                    <template v-slot:rodape>
+                        <button type="button" class="btn btn-primary btn-sm float-right">Adicionar</button>
+                    </template>
+                </card-component>
+                <!-- fim do card de listagem de marcas -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
