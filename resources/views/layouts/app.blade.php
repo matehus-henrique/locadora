45ae35e96c3fd5df67eb2cab6d4f226cde664e18
matehus-henrique <mtheusriki@gmail.com>
2022-10-24 14:39:20 -0300

    Extra - Adicionando um breadcrumb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,24 +33,25 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                       @auth
-                       <li class="nav-item">
-                            <a class="nav-link" href="#">Cliente</a>
-                       </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Locações</a>
-                       </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Veiculos</a>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#">Carros</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{ route('marcas')}}">Marcas</a>
-                                <a class="dropdown-item" href="#">Modelos</a>
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Clientes</a>
+                            </li>
 
-                            </div>
-                       </li>
-                       @endauth
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Locações</a>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Veículos</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Carros</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="{{ route('marcas') }}">Marcas</a>
+                                    <a class="dropdown-item" href="#">Modelos</a>
+                                </div>
+                            </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -91,6 +92,15 @@
                 </div>
             </div>
         </nav>
+        
+        @auth
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">{{Route::currentRouteName()}}</li>
+                </ol>
+            </nav>
+        @endauth
 
         <main class="py-4">
             @yield('content')
